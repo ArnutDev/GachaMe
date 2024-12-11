f1772a79a6dc916d42f3550be53456846ac5698c
ArnutDev <arnutbuadonpai@gmail.com>
2024-12-11 21:27:08 +0700

    Delete test-api.js

--- public/src/test-api.js
+++
@@ -1,6 +0,0 @@
-fetch('https://api.rangers.lerico.net/data') // URL ของ API
-    .then(response => response.json())
-    .then(data => {
-        console.log(data); // แสดงข้อมูลที่ได้รับจาก API
-    })
-    .catch(error => console.error('Error:', error));
